051e1b71246278f1882d6d1fd11ac4fb4094219b
rnemyrka <rnemyrka@byteant.com>
2019-11-12 00:28:20 +0200

    Updates

---
+++ precache-manifest.755888e3657f69633105d43ff69359ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b788843c138c2de45512ef5cd0b207a",
+    "url": "/animals-search/index.html"
+  },
+  {
+    "revision": "b9b3c5d7daab746805af",
+    "url": "/animals-search/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a10fe9c42fa4a5e322e0",
+    "url": "/animals-search/static/css/main.39fae60d.chunk.css"
+  },
+  {
+    "revision": "b9b3c5d7daab746805af",
+    "url": "/animals-search/static/js/2.4a8700b4.chunk.js"
+  },
+  {
+    "revision": "a10fe9c42fa4a5e322e0",
+    "url": "/animals-search/static/js/main.64479b7e.chunk.js"
+  },
+  {
+    "revision": "d4e310734a18a69ed764",
+    "url": "/animals-search/static/js/runtime-main.cf46fc03.js"
+  }
+]);
